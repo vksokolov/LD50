cbd2e17f38708fb6dfd2d43596f655041a22f238
Vladislav Sokolov <vladislav.k.sokolov@gmail.com>
2022-09-24 20:50:55 +0300

    Resources are ReactiveProperties now

--- Assets/Scripts/ResourceService/ResourceService.cs
+++ Assets/Scripts/ResourceService/ResourceService.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using UniRx;
 
 public class ResourceService
 {
-    private readonly Dictionary<ResourceType, int> _resources;
+    private readonly Dictionary<ResourceType, ReactiveProperty<int>> _resources;
     public ResourceService()
     {
-        _resources = new Dictionary<ResourceType, int>();
+        _resources = new Dictionary<ResourceType, ReactiveProperty<int>>();
         foreach (var resourceType in Enum.GetValues(typeof(ResourceType)))
         {
-            _resources.Add((ResourceType)resourceType, 0);
+            _resources.Add((ResourceType)resourceType, new ReactiveProperty<int>(0));
         }
     }
 
-    public int GetResource(ResourceType type) => _resources[type];
-    public void AddResource(ResourceType type, int value) => _resources[type] += value;
+    public int GetResource(ResourceType type) => _resources[type].Value;
+    public void AddResource(ResourceType type, int value) => _resources[type].Value += value;
 }
